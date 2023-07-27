f02943c6f71beb4fc2d3e4ecdf4243d4874b9c98
Tae-hyeon Kim <simoltobella@gmail.com>
2023-07-27 22:50:48 +0900

    Add: TabView-3
    각 탭 클릭 시 선택(표현)됨

--- src/views/TabView.js
+++ src/views/TabView.js
@@ -7,11 +7,23 @@ const TabView = Object.create(View)
 TabView.setup = function (el) {
   this.init(el)
 
-  this.setActiveTab()
+  this.setActiveTab('추천 검색어')
+  this.bindEvents()
 }
 
-TabView.setActiveTab = function () {
-  this.el.children[0].classList.add('active')
+TabView.setActiveTab = function (tabName) {}
+Array.from(this.el.children).forEach(li => {
+  li.className = li.textContent === tabName ? 'active' : ''
+})
+
+TabView.handleClick = function (content) {
+  this.setActiveTab(content)
+}
+
+TabView.bindEvents = function () {
+  Array.from(this.el.children).forEach(li => {
+    li.addEventListener('click', e => this.handleClick(e.target.textContent))
+  })
 }
 
 export default TabView
